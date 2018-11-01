fdc0383e941bb660691278e1698433ee019747f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 12:37:48 -0400

    Country Club 1.0  /main.js

--- main.js
+++ main.js
@@ -146,6 +146,10 @@ require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vu
                             {
                                 name: "nav.hours",
                                 href: "/hours"
+                            },
+                            {
+                                name: "nav.careers",
+                                href: "/hours"
                             }
                         ]
                     }
