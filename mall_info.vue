295d274cdc1dbc43b229be0952f9acb97a22cb6d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 13:18:24 -0400

    Country Club 1.0  /mall_info.vue

--- mall_info.vue
+++ mall_info.vue
@@ -87,11 +87,13 @@
                 pages() {
                     var pages_json = [];
                     pages_json = _.concat(pages_json, this.leasingContent, this.marketingContent, this.areaContent)
+                    if(pages_json && pages_json.length>0) {
                     _.forEach(pages_json, function (value, key) {
                         if ( _.includes([2], key)) {
                             value.isLast = true;
                         }
                     });
+                    }
                     return pages_json
                 },
                 dropDownSelect() {
