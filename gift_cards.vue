2e3a6bedd08833ad73fe43d93c3c5628a4253fa2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 12:55:00 -0400

    Country Club 1.0  /gift_cards.vue

---
+++ gift_cards.vue
@@ -0,0 +1,117 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loader v-if="!dataLoaded"></loader>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        			<div class="site_container">
+        				<div class="header_content caps">
+        					<h1>{{ $t("programs_page.programs_header") }}</h1>
+        				</div>
+        			</div>
+        		</div>
+        		<div class="site_container">
+                    <div class="program_header_container hidden_phone">
+        				<div class="program_button_container active">
+    				        {{ $t("programs_page.scoop_title") }}
+        				</div>
+        				<router-link to="/kids-club">
+        				    <div class="program_button_container animated_btn">
+        				        {{ $t("programs_page.kids_club_title") }}
+        				    </div>
+    				    </router-link>
+        			</div>
+        			<div class="visible_phone margin_20">
+        			    <label style="display: none;" for="pageSelect">Select a Page</label>
+        				<v-select v-model="selected" :options="pageOptions" :searchable="false" :on-change="selectPage" class="category-select" inputId="pageSelect"></v-select>
+        			</div>
+        			<div v-if="pageHeader" class="row">
+        			    <div class="col-md-12">
+        			        <h4 class="margin_40 center bold" v-html="pageHeader.body"></h4>
+        			    </div>
+        			</div>
+        			<div class="row">
+        			    <div class="col-md-8">
+        			        <div class="" v-html="pageBody.body"></div>
+        			    </div>
+        			   <div class="col-md-4">
+        			       <img class="max_width" src="http://placehold.it/744x980" alt="" />
+        			   </div>
+        		    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "json!site.json"], function(Vue, Vuex, Site) {
+        return Vue.component("scoop-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    pageBanner: null,
+                    pageHeader: null,
+                    pageBody: null,
+                    pageImage: null,
+                    pageOptions: [
+                        { 'label': 'Southland Scoop', 'value': 'scoop', 'disabled': true },
+                        { 'label': 'Kids Club', 'value': 'club', 'disabled': false }
+                    ],
+                    selected: { 'label': 'Southland Scoop', 'value': 'scoop', 'disabled': true },
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Kids Club Banner');
+                    if (temp_repo) {
+                        try {
+                            this.pageBanner = temp_repo.images[0];
+                        } catch(e) {
+                            
+                        }
+                    } else {
+                        this.pageBanner = { "image_url": "https://via.placeholder.com/1920x300" }
+                    }
+                    
+                    if (response[1]) {
+                        try {
+                            this.pageHeader = response[1].data
+                            this.pageBody = response[1].data.subpages[0]
+                            this.pageImage = "//www.mallmaverick.com"
+                        } catch (e) {
+                            
+                        }
+                    }
+                    
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName'
+                ])
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/southland-southland-scoop.json"})]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                selectPage(item) {
+                    var item = item.value
+                    if (_.includes(item, "club")) {
+                        this.$router.push("/kids-club");
+                    }
+                    
+                }
+            }
+        });
+    });
+</script>
