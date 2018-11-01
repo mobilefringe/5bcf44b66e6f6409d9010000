417b7d924b63f0e578dac91bb37cd05953640754
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 12:51:53 -0400

    Country Club 1.0  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -99,7 +99,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/southland-kids-club.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+Site.subdomain+"-kids-club.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
