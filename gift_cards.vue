4bb973a62ac81bde0e369e8e3c25377600380044
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 12:55:35 -0400

    Country Club 1.0  /gift_cards.vue

--- gift_cards.vue
+++ gift_cards.vue
@@ -98,7 +98,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/southland-southland-scoop.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+Site.subdomain+"-southland-scoop.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
