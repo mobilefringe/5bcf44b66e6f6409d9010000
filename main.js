228594acd36d1539f80c0ee91c7a53c70e0fe245
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-02 12:39:02 -0400

    Country Club 1.0  /main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ require.config({
         'vue-masonry-plugin' : 'https://cdn.jsdelivr.net/npm/vue-masonry@0.10.16/dist/vue-masonry-plugin',
         'vue-paginate': 'https://cdnjs.cloudflare.com/ajax/libs/vue-paginate/3.5.1/vue-paginate',
         // 'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
-        'bootstrap-vue' : 'https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue',
+        'bootstrap-vue' : 'https://codecloud.cdn.speedyrails.net/sites/59347e776e6f64538f150000/text/javascript/1554736414383/bootstrap-vue',
         'campaignMonitor': 'https://js.createsend1.com/javascript/copypastesubscribeformlogic',
         // Dependencies for Map //
         'mousewheel' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min',
