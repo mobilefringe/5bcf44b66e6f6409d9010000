162c28fee59c43a66d8e29114ff00c7e042b33df
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-07-04 17:38:12 -0400

    Country Club 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -34,7 +34,7 @@
                         	    <div class="store_logo_container">
                         	        <router-link :to="'/stores/'+ store.slug">
                             			<div v-if="!store.no_store_logo">
-                            			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5bcf44b66e6f6409d9010000/image/jpeg/1562270790000/CountryClub-Home-Inside Banner-02.jpg"  alt="">
+                            			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png"  alt="">
                             			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
                             			</div>
                             			
