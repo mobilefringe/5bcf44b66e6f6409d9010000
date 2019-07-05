d3aedb18b7c254859dc2bab92c76562b25a7382d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-05 10:10:56 -0400

    Country Club 1.0  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -73,7 +73,7 @@
                             
                         }
                     } else {
-                        this.pageBanner = { "image_url": "https://via.placeholder.com/1920x300" }
+                        this.pageBanner = { "image_url": "" }
                     }
                     
                     // if (response && response[1]) {
