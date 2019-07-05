87777ae779ed2fdc62117975bf24b7c0213a5523
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-05 10:10:40 -0400

    Country Club 1.0  /gift_cards.vue

--- gift_cards.vue
+++ gift_cards.vue
@@ -72,7 +72,7 @@
                             
                         }
                     } else {
-                        this.pageBanner = { "image_url": "https://via.placeholder.com/1920x300" }
+                        this.pageBanner = { "image_url": "" }
                     }
                     
                     if (response && response[1]) {
