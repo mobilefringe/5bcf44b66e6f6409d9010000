2d10ae1d63411f79bd1599335fed22884be0c668
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-08 14:15:25 -0400

    Country Club 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -81,7 +81,7 @@
                     dataLoaded: false,
                     pageBanner : null,
                     search_result : null,
-                    storeFilter: 6439
+                    storeFilter: 6530
                 }
             },
             created (){
