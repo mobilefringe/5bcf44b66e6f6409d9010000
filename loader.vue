64fdb69dcc0fca67973c64b76264c2e5aa457a9b
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-15 17:09:52 -0400

    Country Club 1.0  /loader.vue

---
+++ loader.vue
@@ -0,0 +1,59 @@
+<template>
+    <div v-if="dataLoaded" id="overlay">
+        <div class="loading-container">
+            <vue-simple-spinner size="huge" :message="property.name + 'is loading...'"></vue-simple-spinner>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue-simple-spinner"], function (Vue, Vuex, Spinner) {
+        Vue.component("vue-simple-spinner", Spinner);
+        return Vue.component("loader", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false
+                };
+            },
+            created (){
+                // this.loadData().then(response => {
+                    this.dataLoaded = true;
+                // });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            }
+        });
+    });
+</script>
+
+<style>
+    #overlay {
+        position: fixed;
+        /* Sit on top of the page content */
+        width: 100%;
+        /* Full width (cover the whole page) */
+        height: 100%;
+        /* Full height (cover the whole page) */
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ffffff;
+        z-index: 999;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -moz-box;
+        display: -moz-flex;
+        display: -ms-flexbox;
+        display: flex;
+    }
+    .loading-container .vue-simple-spinner-text{
+        line-height: 1;
+        font-size:2rem;
+    }
+</style>
