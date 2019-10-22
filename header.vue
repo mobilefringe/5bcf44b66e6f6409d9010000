4bcbb8a310701acb4ac521831d9c0fef4990be0b
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-22 18:00:42 -0400

    Country Club 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -55,12 +55,25 @@
 			<div class="row">
 			    <div class="col-md-2 hidden_phone"></div>
 				<div class="col-sm-12 col-md-8">
+					<!--<nav id="primary_nav" class="hidden_phone">-->
+					<!--	<ul>-->
+					<!--	    <li class="menu_item" v-for="item in menu_items" :id="item.id">-->
+					<!--	        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>-->
+					<!--	        <span v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>-->
+					<!--	        <ul v-if="item.sub_menu">-->
+					<!--	            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">-->
+					<!--	                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>-->
+					<!--	            </li>-->
+					<!--			</ul>-->
+					<!--	    </li>-->
+					<!--	</ul>-->
+					<!--</nav>-->
 					<nav id="primary_nav" class="hidden_phone">
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>
-						        <span v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>
-						        <ul v-if="item.sub_menu">
+						        <span tabindex=0 v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>
+						        <ul v-if="item.sub_menu" class="subdropdown">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
 						            </li>
