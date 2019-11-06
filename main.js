f1314f2b8c9c04b3bd2c9baedb08770ee3df772a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-06 09:42:28 -0500

    Country Club 1.0  /main.js

--- main.js
+++ main.js
@@ -93,14 +93,14 @@ require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vu
                                 name: "nav.stores",
                                 href: "/stores"
                             },
-                            {
-                                name: "nav.dine",
-                                href: "/dine"
-                            },
-                            {
-                                name: "nav.play",
-                                href: "/play"
-                            },
+                            // {
+                            //     name: "nav.dine",
+                            //     href: "/dine"
+                            // },
+                            // {
+                            //     name: "nav.play",
+                            //     href: "/play"
+                            // },
                             {
                                 name: "nav.centre_map",
                                 href: "/map"
