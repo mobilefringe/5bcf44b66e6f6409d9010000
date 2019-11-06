6f056d51ff38237104d2f9a48c8978d018d42184
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-06 10:59:09 -0500

    Country Club 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -38,8 +38,14 @@
             created() {
                 this.updateCurrentPage(this.id);
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Page Banner');
-                    if(temp_repo && temp_repo.images) {
+                    var temp_repo = ""
+                    if (_.includes(this.$route.path, "-leasing")) {
+                        temp_repo = this.findRepoByName('Mall Info Banner');
+                    } else {
+                        temp_repo = this.findRepoByName('Page Banner');
+                    }
+                    
+                    if (temp_repo && temp_repo.images) {
                         try {
                             this.pageBanner = temp_repo.images[0];
                         } catch(e) {
