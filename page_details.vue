e5ee41586f74119bdf74315c37d033ef1a072c12
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-06 11:47:43 -0500

    Country Club 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -91,7 +91,6 @@
                 loadData: async function() {
                     try {
                         let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
-                        
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
