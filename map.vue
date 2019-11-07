9cda32352aecfbd7dc677656abc8acb84d07364d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-07 17:07:44 -0500

    Country Club 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -21,12 +21,9 @@
                                 </search-component>
                                 <i id="store_search_icon" class="fa fa-search" aria-hidden="true"></i>
                             </div>
-                            <div class="margin-30 visible-mobile"></div>
-                            <div class="hidden_phone">
-                                <div class="store_list_container hidden-mobile">
-                                    <div class="store_name" v-for="store in allStores">
-                                        <p v-on:click="dropPin(store)">{{store.name}}</p>
-                                    </div>
+                            <div class="store_list_container hidden_phone">
+                                <div class="store_name" v-for="store in allStores">
+                                    <p v-on:click="dropPin(store)">{{store.name}}</p>
                                 </div>
                             </div>
                         </div>
@@ -34,19 +31,6 @@
                             <mapplic-map ref="mapplic_ref" :height="556" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-map>
                         </div>
                     </div>
-                    <!--<div class="row">-->
-                    <!--    <div class="col-md-9 col-md-push-3">-->
-                            
-                    <!--    </div>-->
-                    <!--    
-                        <!--<div class="hidden_phone col-md-3 col-md-pull-9">-->
-                        <!--    <div class="store_list_container hidden-mobile">-->
-                        <!--        <div class="store_name" v-for="store in allStores">-->
-                        <!--            <p v-on:click="dropPin(store)">{{store.name}}</p>-->
-                        <!--        </div>-->
-                        <!--    </div>-->
-                        <!--</div>-->
-                    <!--</div>-->
                 </div>
             </div>
         </transition>
