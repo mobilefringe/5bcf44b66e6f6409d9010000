8066eb78de232cbf53920209dbcc0aaead0785a3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-03-02 16:25:25 -0500

    Country Club 1.0  /main.js

--- main.js
+++ main.js
@@ -149,10 +149,10 @@ require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vu
                                 name: "nav.hours",
                                 href: "/hours"
                             },
-                            {
-                                name: "nav.leasing",
-                                href: "/pages/countryclub-leasing"
-                            },
+                            // {
+                            //     name: "nav.leasing",
+                            //     href: "/pages/countryclub-leasing"
+                            // },
                             {
                                 name: "nav.jobs",
                                 href: "/jobs"
