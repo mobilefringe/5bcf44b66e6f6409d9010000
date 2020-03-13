7da4180397a527d128e7a11ee133e434bc2a0042
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-13 17:02:01 -0400

    Country Club 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -8,13 +8,11 @@
         			<div class="home_banner_container">
         				<slick ref="slick" :options="slickOptions">
         					<div class="" v-for="banner in banners" v-if="banners">
-        						<div class="home_banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
-        						    <!--<div class="home_banner_content" v-if="banner.name && banner.description">-->
-        						    <!--    <h1>{{ banner.name }}</h1>-->
-        						    <!--    <h2>{{ banner.description }}</h2>-->
-        						    <!--</div>   -->
-        						</div>
-        					</div>
+                                <a v-if="banner.url" :href="banner.url">
+                                    <div class="home_banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                </a>
+                                <div v-else class="home_banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                            </div>
         				</slick>
         			</div>
         		</div>
