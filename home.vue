fec3a5be5b0f90c072693de818bbce08ddaa0a8d
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-05-13 17:07:22 -0400

    Country Club 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -109,6 +109,24 @@
         return Vue.component("home-component", {
             template: template, // the variable template will be injected
             props:['locale'],
+            head: {
+                // To use "this" in the component, it is necessary to return the object through a function
+                title: function () {
+                  return {
+                    inner: this.meta.meta_title,
+                    separator: ' ', // Leave empty separator
+                    complement: ' ' // Leave empty complement
+                  }
+                },
+                meta: function () {
+                  return [
+                     { name: 'description', id: 'description', content: this.meta.meta_description },
+                     { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
+                     { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
+                     { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
+                  ]
+                }
+            },
             data: function() {
                 return {
                     dataLoaded: false,
